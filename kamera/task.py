96e298d74a82ee90f4484b23c74158e353a74550
eirki <eirik.b.stavest@gmail.com>
2018-04-21 22:25:53 +0200

    remove pytz import from config

--- kamera/task.py
+++ kamera/task.py
@@ -34,7 +34,7 @@ def parse_date(entry: KameraEntry) -> dt.datetime:
             local_date = utc_date.astimezone(tz=pytz.timezone(img_tz))
             return local_date
 
-    local_date = utc_date.astimezone(tz=config.default_tz)
+    local_date = utc_date.astimezone(tz=pytz.timezone(config.default_tz))
     return local_date
 
 
