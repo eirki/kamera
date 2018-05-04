adf8d6fb5485e3a23d8442fce598a82f9697660a
eirki <eirik.b.stavest@gmail.com>
2018-05-04 16:20:33 +0200

    remove file handling from logger

--- kamera/logger.py
+++ kamera/logger.py
@@ -1,11 +1,6 @@
 #! /usr/bin/env python3.6
 # coding: utf-8
-import sys
 import logging
-from logging.handlers import RotatingFileHandler
-from pathlib import Path
-
-from kamera import config
 
 log = logging.getLogger()
 log.setLevel(logging.INFO)
@@ -13,19 +8,6 @@ log.setLevel(logging.INFO)
 formatter = logging.Formatter(
     "[%(asctime)s] %(filename)s %(levelname)s - %(message)s"
 )
-
-log_name = Path(sys.argv[0]).stem
-log_path = config.home / "log" / f"{log_name}.log"
-
-fh = RotatingFileHandler(
-    filename=log_path.as_posix(),
-    maxBytes=200000,
-    backupCount=5
-)
-
-fh.setLevel(logging.INFO)
-fh.setFormatter(formatter)
-log.addHandler(fh)
 
 ch = logging.StreamHandler()
 ch.setLevel(logging.INFO)
