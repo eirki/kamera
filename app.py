402707698908a7ae6c19a5bcc184ada585f312c8
eirki <eirik.b.stavest@gmail.com>
2018-09-11 10:03:26 +0200

    Use built-in rq dashboard auth

--- app.py
+++ app.py
@@ -3,17 +3,17 @@
 from kamera.logger import log
 
 import sys
-import os
 from hashlib import sha256
 import hmac
 import json
 import datetime as dt
 from pathlib import Path
 
-from flask import Flask, request, abort, Response, g
+from flask import Flask, request, abort, g
 import redis
 import rq
 import rq_dashboard
+from rq_dashboard.cli import add_basic_auth
 import redis_lock
 import dropbox
 
@@ -38,34 +38,11 @@ def get_redis_client():
     return redis_client
 
 
-# Define and apply rq-dashboard authenication,
-# from https://github.com/eoranged/rq-dashboard/issues/75
-def check_auth(username, password) -> bool:
-    return (
-        username == config.rq_dashboard_username
-        and password == config.rq_dashboard_password
-    )
-
-
-def basic_auth() -> Optional[Response]:
-    """Ensure basic authorization."""
-    error_resp = Response(
-        "Could not verify your access level for that URL.\n"
-        "You have to login with proper credentials",
-        401,
-        {"WWW-Authenticate": 'Basic realm="Login Required"'},
-    )
-
-    auth = request.authorization
-    log.debug(auth)
-    return (
-        error_resp if not (auth or check_auth(auth.username, auth.password)) else None
-    )
-
-
 app.config.from_object(rq_dashboard.default_settings)
 app.config["REDIS_URL"] = config.redis_url
-rq_dashboard.blueprint.before_request(basic_auth)
+add_basic_auth(
+    rq_dashboard.blueprint, config.rq_dashboard_username, config.rq_dashboard_password
+)
 app.register_blueprint(rq_dashboard.blueprint, url_prefix="/rq")
 
 
