1d39b136ebb7e33f99c703dac8f48de33ae560a3
eirki <eirik.b.stavest@gmail.com>
2019-03-07 13:08:26 +0100

    Simplify logging

--- kamera/logger.py
+++ kamera/logger.py
@@ -1,13 +1,15 @@
 #! /usr/bin/env python3.6
 # coding: utf-8
-import logging
+
 import os
+import logging
+from logging import StreamHandler
+
 
 log = logging.getLogger()
-log.setLevel(os.environ.get("LOGLEVEL", "INFO"))
-
 formatter = logging.Formatter("%(filename)s %(levelname)s - %(message)s")
-ch = logging.StreamHandler()
-ch.setLevel(os.environ.get("LOGLEVEL", "INFO"))
-ch.setFormatter(formatter)
-log.addHandler(ch)
+
+handler = StreamHandler()
+handler.setFormatter(formatter)
+log.addHandler(handler)
+log.setLevel(os.environ.get("loglevel", "INFO"))
